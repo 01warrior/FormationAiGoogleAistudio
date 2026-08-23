e9fc83bcb34a1b83e7f02015163c60fa6c2c6514
Soumaila Savadogo <134203417+01warrior@users.noreply.github.com>
2026-08-23 22:14:27 +0100

    icone

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig(() => {
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['icon-192.png', 'icon-512.png', 'icon-144.png'],
         manifest: {
           name: 'VerbMaster',
           short_name: 'VerbMaster',
@@ -19,25 +18,7 @@ export default defineConfig(() => {
           theme_color: '#ffffff', // Primary color
           background_color: '#F8FAFC', // Background color
           display: 'standalone',
-          start_url: '/',
-          icons: [
-            {
-              src: '/icon-144.png',
-              sizes: '144x144',
-              type: 'image/png'
-            },
-            {
-              src: '/icon-192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: '/icon-512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable'
-            }
-          ]
+          start_url: '/'
         },
         workbox: {
           maximumFileSizeToCacheInBytes: 3000000
